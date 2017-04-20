88315e6dfdae52a09a39a93355af3fa93e6bf166
Francisco Gutierrez <francisco.gutierrez@cs.kuleuven.be>
2017-04-20 22:51:52 +0200

    added example

--- similar.py
+++ similar.py
@@ -1,3 +1,13 @@
-def jaccard_index(set_1, set_2):
-    n = len(set_1.intersection(set_2))
-    return n / float(len(set_1) + len(set_2) - n)
+import tfidf
+
+def jaccard_index(set1, set2):
+    n = len(set1.intersection(set2))
+    return n / float(len(set1) + len(set2) - n)
+
+
+table = tfidf.tfidf()
+table.addDocument("foo", ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel"])
+table.addDocument("bar", ["alpha", "bravo", "charlie", "india", "juliet", "kilo"])
+table.addDocument("baz", ["kilo", "lima", "mike", "november"])
+
+print table.similarities (["alpha", "bravo", "charlie"]) # => [['foo', 0.6875], ['bar', 0.75], ['baz', 0.0]]
