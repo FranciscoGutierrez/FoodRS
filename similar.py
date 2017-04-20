2a86ed506541200de312b42fb2394420181d7d70
Francisco Gutierrez <francisco.gutierrez@cs.kuleuven.be>
2017-04-20 22:55:46 +0200

    Added mongodb database

--- similar.py
+++ similar.py
@@ -1,13 +1,16 @@
 import tfidf
+from pymongo import MongoClient
 
 def jaccard_index(set1, set2):
     n = len(set1.intersection(set2))
     return n / float(len(set1) + len(set2) - n)
 
+def tfidf():
+    table = tfidf.tfidf()
+    table.addDocument("foo", ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel"])
+    table.addDocument("bar", ["alpha", "bravo", "charlie", "india", "juliet", "kilo"])
+    table.addDocument("baz", ["kilo", "lima", "mike", "november"])
+    return table.similarities (["alpha", "bravo", "charlie"]) # => [['foo', 0.6875], ['bar', 0.75], ['baz', 0.0]]
 
-table = tfidf.tfidf()
-table.addDocument("foo", ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel"])
-table.addDocument("bar", ["alpha", "bravo", "charlie", "india", "juliet", "kilo"])
-table.addDocument("baz", ["kilo", "lima", "mike", "november"])
-
-print table.similarities (["alpha", "bravo", "charlie"]) # => [['foo', 0.6875], ['bar', 0.75], ['baz', 0.0]]
+def mongodb():
+    client = MongoClient('mongodb://localhost:27017/')
