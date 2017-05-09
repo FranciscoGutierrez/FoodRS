e79752bcbc683a907e8aa9e2c24962da1104b84a
Francisco Gutiérrez <francisco.gutierrez@cs.kuleuven.be>
2017-05-09 13:45:37 +0200

    Added cluster analysis

--- scripts/init.py
+++ scripts/init.py
@@ -3,9 +3,14 @@ import pprint
 import rsystem
 import keys
 
-products = list(rsystem.getProducts(keys.URI))
-rsystem.deleteAllSimilar(keys.URI)
+products  = list(rsystem.getProducts(keys.URI))
+favorites = list(rsystem.getLastUser(keys.URI))[0]["favorites"]
+p_fav     = list(rsystem.getProductsWithID(keys.URI, favorites))
+
 rsystem.deleteAllAlternative(keys.URI)
+rsystem.alternative_products(keys.URI, p_fav, products)
+
+rsystem.deleteAllSimilar(keys.URI)
 rsystem.similar_products(keys.URI, products)
 
 
--- scripts/rsystem.py
+++ scripts/rsystem.py
@@ -4,13 +4,53 @@ from sklearn.metrics import jaccard_similarity_score
 import pprint
 import numpy as np
 
+# Mongo URI: "mongodb://<user>:<password>@ds161400.mlab.com:61400/healthyfoods"
 def getProducts(uri):
-    # Mongo URI: "mongodb://<user>:<password>@ds161400.mlab.com:61400/healthyfoods"
     client = MongoClient(uri)
     db = client['healthyfoods']
     products = db.products
-    print "==== Products Acquired ===="
     return products.find()
+
+def getDrinks(uri):
+    client = MongoClient(uri)
+    db = client['healthyfoods']
+    products = db.products
+    list = ["en:beverages","en:carbonated-drinks","en:sodas","en:sugared-beverages"]
+    return products.find({"categories_tags": { "$in": list }})
+
+def getVeggiesFruits(uri):
+    client = MongoClient(uri)
+    db = client['healthyfoods']
+    products = db.products
+    return products.find({"categories_tags": "en:plant-based-foods"})
+
+def getProteins(uri):
+    client = MongoClient(uri)
+    db = client['healthyfoods']
+    products = db.products
+    list = ["en:meals", "en:meats","en:seafood","en:dairies","en:cheeses", "fr:salade-de-poulet-curry", "en:soupe","en:seafood","en:fishes"]
+    return products.find({"categories_tags": { "$in": list }})
+
+def getSnacksCandies(uri):
+    client = MongoClient(uri)
+    db = client['healthyfoods']
+    products = db.products
+    list = ["en:desserts","en:salty-snacks", "en:waffles", "en:sugary-snacks", "en:chocolates"]
+    return products.find({"categories_tags": { "$in": list }})
+
+def getLastUser(uri):
+    client = MongoClient(uri)
+    db = client['healthyfoods']
+    users = db.users
+    print "==== User Acquired ===="
+    return users.find().sort("_id",1).limit(1)
+
+def getProductsWithID(uri, list):
+    client = MongoClient(uri)
+    db = client['healthyfoods']
+    products = db.products
+    print "==== Fav Products Acquired ===="
+    return products.find({"_id": {"$in": list}})
 
 # Discover things, explore similar products... not related to user profile.
 def similar_products(uri, products):
@@ -39,23 +79,23 @@ def similarity_test(a,b):
     return jaccard_similarity_score(a,b)
 
 # Based in the products the user usually likes, find healthy alternatives.
-def alternative_products(uri, list):
+def alternative_products(uri, favorites, products):
     client = MongoClient(uri)
     db = client['healthyfoods']
     output = []
     for a in products:
-        test_a = a["_keywords"]
+        score = 0.0
+        key_a = a["_keywords"]
         table = []
-        for b in products:
-            test_b = b["_keywords"]
-            if len(test_a) > 0 and len(test_b) > 0:
-                score = jaccard_similarity_score(test_a, test_b)
-            else:
-                score = 0.0
-            if(score < 1.0): # and > 0.5 to get the most similar products.
+        for b in favorites:
+            key_b   = b["_keywords"]
+            key_max = max(len(key_a), len(key_b))
+            key_a = key_a + [0]*(key_max - len(key_a))
+            key_b = key_b + [0]*(key_max - len(key_b))
+            score = jaccard_similarity_score(key_a, key_b)
+            if(score < 1.0 and score > 0.5): # and > 0.5 to get the most similar products.
                 table.append({"_id": b["_id"], "score": score})# "product": b})
-        output.append({"_id": test_a, "similarity" : table})
-    pprint.pprint(output)
+        output.append({"_id": a["_id"], "similarity" : table})
     result = db.alternative.insert_many(output)
     return 0;
 
