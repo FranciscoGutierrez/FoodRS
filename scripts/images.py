f17a2a7f5d2135e9d960fb62eefa804d6b5f283c
Francisco Gutiérrez <francisco.gutierrez@cs.kuleuven.be>
2017-05-15 11:11:21 +0200

    Updated script to get images

--- scripts/images.py
+++ scripts/images.py
@@ -2,6 +2,7 @@ import json
 import urllib2
 import keys
 import csv
+import requests
 
 from pymongo import MongoClient
 
@@ -11,7 +12,7 @@ products = list(db.products.find())
 table = []
 
 for a in products:
-    print a["_id"]
+    # print a["_id"]
     table.append(a["_id"])
 
 with open('eggs.csv', 'w') as csvfile:
@@ -22,5 +23,11 @@ with open('eggs.csv', 'w') as csvfile:
         try:
             resp = json.load(urllib2.urlopen(url))
             writer.writerow({'id': id, 'url': resp["product"]["image_url"]})
+            f = open("photos/"+id+'.jpg','wb')
+            f.write(requests.get(resp["product"]["image_url"]).content)
+            f.close()
         except Exception as e:
             writer.writerow({'id': id, 'url': "demo/d1.png"})
+            f = open("photos/"+id+'.jpg','wb')
+            f.write(requests.get("http://localhost:3000/demo/d1.png").content)
+            f.close()
